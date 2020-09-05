6a449149f879d2ef540f0c63dc6884b19470e737
varsharathore <rathorevarsha98@gmail.com>
2020-09-05 09:49:25 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function changeBg(coverImg) {
+    document.querySelector('.laysType').src = coverImg;    
+}
+
+function blackBg() {
+    document.body.style.backgroundImage = "url('https://i.imgur.com/jbx07AD.jpg')";
+}
+function blueBg() {
+    document.body.style.backgroundImage = "url('https://i.imgur.com/QWCNfGP.jpg')";
+}
+function greenBg() {
+    document.body.style.backgroundImage = "url('https://i.imgur.com/I6LqeFW.jpg')";
+}
+function pinkBg() {
+    document.body.style.backgroundImage = "url('https://i.imgur.com/aHjyRlT.jpg')";
+}
+function purpleBg() {
+    document.body.style.backgroundImage = "url('https://i.imgur.com/ub9vI5M.jpg')";
+}
+function yellowBg() {
+    document.body.style.backgroundImage = "url('https://i.imgur.com/lpfZGOg.jpg')";
+}
+function redBg() {
+    document.body.style.backgroundImage = "url('https://i.imgur.com/gK1PapL.jpg')";
+}
+function orangeBg() {
+    document.body.style.backgroundImage = "url('https://i.imgur.com/bMefxfY.jpg')";
+}
+
